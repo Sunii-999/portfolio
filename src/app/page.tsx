f2abaa51be0a17471f511e588f52e3a98c878416
Sunii-999 <stijnwal01@gmail.com>
2026-04-10 13:56:06 +0200

    new portfolio setup nextjs and tailwiind

---
+++ src/app/page.tsx
@@ -0,0 +1,9 @@
+import Navbar from "@/components/Navbar/Navbar";
+
+export default function Home() {
+  return (
+      <main className="flex flex-1 w-full  flex-col items-center justify-between py-32 px-16 bg-white  sm:items-start">
+        <Navbar />
+      </main>
+  );
+}
