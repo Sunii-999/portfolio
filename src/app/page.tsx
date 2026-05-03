0ef485c9395ed58cfce43df791a50272aeffeeef
Sunii-999 <stijnwal01@gmail.com>
2026-05-03 20:34:09 +0200

    fix: everything

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,7 @@
-import Header from "@/components/Header/Header";
-import Navbar from "@/components/Navbar/Navbar";
-import References from "@/components/References/References";
-
 export default function Home() {
   return (
-      <main className="flex flex-1 w-full  flex-col items-center justify-between py-32 px-16 bg-white  sm:items-start">
-        <Navbar />
-        <Header />
-        <References />
-      </main>
+    <main className="bg-white">
+      <div className="flex"></div>
+    </main>
   );
 }
