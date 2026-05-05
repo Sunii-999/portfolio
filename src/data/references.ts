0de2bf0833fdd669c417d97b55175cfac0ef85ad
Sunii <stijnwal01@gmail.com>
2026-05-05 08:04:58 +0200

    Revise portfolio review for improved readability
    Updated review text for clarity and flow.

--- src/data/references.ts
+++ src/data/references.ts
@@ -21,6 +21,6 @@ export const references = [
       "Student Interior Design and Interior Architecture | Whitecliffe University Berlin School of applied sciences",
     product: "Portfolio website",
     review:
-      "Honestly, this portfolio turned out way better than I expected. It looks super clean and actually feels like something a real designer would have, not just a student project. The vibe is really modern and everything just flows nicely, which made it super easy to send to recruiters.I’m a second-year student in Berlin studying Interior Design and Interior Architecture, and I literally ended up landing an internship thanks to this LOCATED IN JAPAN!!!!!! Got multiple compliments on the site too. Couldn’t be happier with how it turned out.",
+      "This portfolio turned out way better than I expected. It looks super clean and actually feels like something a real designer would have, not just a student project. The vibe is exactly what i wanted, everything just flows nicely, which made it super easy to send to recruiters.I literally ended up landing an internship thanks to this LOCATED IN JAPAN!!!!!! Got multiple compliments on the site too.",
   },
 ];
